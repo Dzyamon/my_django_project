57a9090cfb4fbc8ec90bed86cc842a32c20fb3a1
Dzyamon <dmitry.olehnovich@gmail.com>
2023-05-07 00:50:14 +0300

    commit superuser

--- myapp1/models.py
+++ myapp1/models.py
@@ -8,3 +8,9 @@ class Worker(models.Model):
     name = models.CharField(max_length=20, blank=False)
     second_name = models.CharField(max_length=35, blank=False)
     salary = models.IntegerField(default=0)
+
+# python manage.py createsuperuser
+# dzyamon 123
+
+    def __str__(self):
+        return f'{self.second_name} {self.name}'
